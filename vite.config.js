7c5681a55dbdffe6be980ee46433368aefc906e7
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2026-05-27 12:36:16 -0700

    package changes

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { imagetools } from '@zerodevx/svelte-img/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 
 export default defineConfig({
 	plugins: [
 
 		sveltekit(),
-		imagetools()
-		
+		imagetools(),
+		tailwindcss()
+
 	],
 	server: {
 		fs: {
